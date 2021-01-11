357c5233f41977a92a5580ccc12a66634c942f30
lizardcoder9999 <lizardcoder6@gmail.com>
2021-01-11 16:44:48 -0500

    Finished logout operation on the navbar component

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MaterialModule } from '../../material-ui.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
-  declarations: [AdminDashboardComponent, NavbarComponent],
-  imports: [CommonModule],
+  declarations: [AdminDashboardComponent, NavbarComponent, UsersComponent],
+  imports: [CommonModule, MaterialModule, BrowserModule],
   exports: [AdminDashboardComponent],
 })
 export class DashboardModule {}
--- src/app/modules/dashboard/navbar/navbar.component.ts
+++ src/app/modules/dashboard/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { LogoutService } from '../../authentication/services/logout.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private _logout: LogoutService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logoutAdmin() {
+    this._logout.logOutAdmin();
   }
-
 }
