5ff394cda6716b4274ee80893a737c5d3e03dbd3
lizardcoder9999 <lizardcoder6@gmail.com>
2021-01-11 18:17:38 -0500

    Added more security to the users component

--- src/app/modules/authentication/services/login.service.ts
+++ src/app/modules/authentication/services/login.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private _token: TokenService) {}
 
   loginAdmin(admin) {
     return this.http.post<any>('http://127.0.0.1:5000/api/admin/login', admin);
   }
 
   //Logged in method for the auth gaurd
-
+  //
   loggedIn() {
     return !!localStorage.getItem('token');
   }
--- src/app/modules/dashboard/users/users.component.ts
+++ src/app/modules/dashboard/users/users.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenService } from '../../authentication/services/token.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
+  constructor(private _token: TokenService, private _router: Router) {}
 
-  constructor() { }
+  adminId: string;
 
   ngOnInit(): void {
+    this._token.verifyToken().subscribe(
+      (res) => {
+        this.adminId = res.admin._id;
+        localStorage.setItem('adminid', this.adminId);
+      },
+      (err) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 400) {
+            this._router.navigate(['/login']);
+          }
+        }
+      }
+    );
   }
-
 }
